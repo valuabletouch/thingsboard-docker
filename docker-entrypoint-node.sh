b033cfa73cca469db25d107d45ac337438edba60
Ozgun Ay <ozgun.ay@vtcenerji.com>
2024-05-16 11:02:09 +0000

    Upgrade service added

--- docker-entrypoint-node.sh
+++ docker-entrypoint-node.sh
@@ -4,10 +4,6 @@ appName="ThingsBoard Node Microservice"
 
 appCode="thingsboard"
 
-appMainClass="org.thingsboard.server.ThingsboardServerApplication"
-
-installMainClaass="org.thingsboard.server.ThingsboardInstallApplication"
-
 jarFilePath="/app/bin/$appCode.jar"
 
 logbackFilePath="/app/conf/logback.xml"
@@ -32,6 +28,8 @@ if [ "$STARTUP_MODE" == "install" ]; then
 
     echo "Starting '$appName' installation ..."
 
+    installMainClaass="org.thingsboard.server.ThingsboardInstallApplication"
+
     exec java -cp \
         "$jarFilePath" \
         $JAVA_OPTS \
@@ -44,33 +42,30 @@ if [ "$STARTUP_MODE" == "install" ]; then
 
 elif [ "$STARTUP_MODE" == "upgrade" ]; then
 
-    if [[ -z "${UPGRADE_FROM_VERSION// }" ]]; then
-        echo "FROM_VERSION variable is invalid or unspecified!"
-        exit 1
-    else
-        fromVersion="${UPGRADE_FROM_VERSION// }"
-    fi
-
     echo "Starting '$appName' upgrade ..."
+
+    upgradeMainClass="org.thingsboard.server.vsensor.upgrade.ThingsboardUpgradeApplication"
 
     exec java -cp \
         "$jarFilePath" \
         $JAVA_OPTS \
-        -Dloader.main=$installMainClaass \
+        -Dloader.main=$upgradeMainClass \
         -Dspring.jpa.hibernate.ddl-auto=none \
-        -Dinstall.upgrade=true \
-        -Dinstall.upgrade.from_version="$fromVersion" \
+        -Dupgrade.upgrade=true \
         -Dlogging.config="$logbackFilePath" \
         org.springframework.boot.loader.PropertiesLauncher 2>&1 || :
 
+else
+
+    echo "Starting '$appName' ..."
+
+    appMainClass="org.thingsboard.server.ThingsboardServerApplication"
+
+    exec java -cp \
+        "$jarFilePath" \
+        $JAVA_OPTS \
+        -Dloader.main=$appMainClass \
+        -Dspring.jpa.hibernate.ddl-auto=none \
+        -Dlogging.config="$logbackFilePath" \
+        org.springframework.boot.loader.PropertiesLauncher
 fi
-
-echo "Starting '$appName' ..."
-
-exec java -cp \
-    "$jarFilePath" \
-    $JAVA_OPTS \
-    -Dloader.main=$appMainClass \
-    -Dspring.jpa.hibernate.ddl-auto=none \
-    -Dlogging.config="$logbackFilePath" \
-    org.springframework.boot.loader.PropertiesLauncher
